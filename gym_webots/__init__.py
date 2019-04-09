b8edad8d149b768b22f0fd8946e773337967e361
Aayush Gupta <aayu.gupta.96@gmail.com>
2019-04-09 21:18:03 +0530

    initial code

---
+++ gym_webots/__init__.py
@@ -0,0 +1,3 @@
+from gym.envs.registration import register
+
+register(id='wb-4wheels-v0', entry_point='gym_webots.envs:Wb4WheelsEnv')
---
+++ gym_webots/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_webots.envs.wb_4wheels_env import Wb4WheelsEnv
